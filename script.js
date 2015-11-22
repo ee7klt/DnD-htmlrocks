08fc0351f0072ccde7eb25ddfca74171f8ed0165
ee7klt <kltah1@gmail.com>
2015-11-22 21:50:21 +0800

    opacity 40% not working

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function handleDragStart(e) {
-  this.style.opacity = '0.4';
+
+
+function handleclick(e) {
+  console.log("button clicked")
 }
 
-var cols = document.querySelectorAll('#columns .column');
-[].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart,false);
-});
+var el = document.getElementById("a");
+console.log("el = " + el);
+el.addEventListener("click", special, false);
 
-function count_rabbits() {
-  for (var i = 1; i <= 3; i++) {
-    console.log("Rabbit "+i+" out of the hat");
-  }
+function special() {
+  console.log("I am special");
 }
